387d81e6756535200acdb7f716012bd043c918fb
Md Hafiz Al foisal <alfoisal940700@gmail.com>
2025-05-04 17:32:23 +0600

    content update

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <title>Forgot Password</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta content="" name="description" />
+    <meta content="Otithee Hotel & Resort ERP" name="description" />
     <meta content="MD Hafiz Al Foisal" name="author" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
